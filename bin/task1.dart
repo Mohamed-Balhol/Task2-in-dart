34a4c7998b24a4b2ddd78b2853fe782e55796fca
Mohamed Balhol <mhack@gmail.com>
2025-04-18 22:13:49 +0300

    add try catch and throw Handling

--- bin/task1.dart
+++ bin/task1.dart
@@ -2,40 +2,58 @@ import 'dart:ffi';
 import 'dart:io';
 
 void main() {
-  stdout.write('Enter Opration + ,-,*,/,%,!: ');
-  String? opration = stdin.readLineSync();
+  String? opration;
+  try {
+    stdout.write('Enter Opration + ,-,*,/,%,!: ');
+    opration = stdin.readLineSync();
+    if (opration != '+' &&
+        opration != '-' &&
+        opration != '*' &&
+        opration != '/' &&
+        opration != '%' &&
+        opration != '!') {
+      exit(1);
+    }
+  } catch (e) {
+    print(e);
+  }
   //التحقق من نوع العملية التي ادخلها المستخدم
   if (opration == '!') {
-    int x;
-    stdout.write('Enter The Number ');
-    x = int.parse(stdin.readLineSync().toString());
-    print(factorial(x));
+    try {
+      int x;
+      stdout.write('Enter The Number ');
+      x = int.parse(stdin.readLineSync().toString());
+      print(factorial(x));
+    } catch (e) {
+      print(e);
+    }
   }
   //////////////////////////////////
   else {
     calculator(opration);
   }
 }
+
 //دالة لايجاد المضروب النوني
 int factorial(x) {
   if (x == 0) {
     return 1;
+  } else if (x < 0) {
+    throw Error.err;
   }
 
   return x * factorial(x - 1);
 }
+
 //دالة للقيام بالعمليات الحسابية
 void calculator(opr) {
-  dynamic x, y, result;
-  print('Enter The Number1: ');
-  x = int.parse(stdin.readLineSync().toString());
-  print('Enter The Number2: ');
-  y = int.parse(stdin.readLineSync().toString());
-  if (opr == '/' && y == 0) {
-    print('please , Enter correct number! ; You cant enter Zero in the position');
-    print('try again please');
-    calculator(opr);
-  } else {
+  try {
+    dynamic x, y, result;
+    print('Enter The Number1: ');
+    x = int.parse(stdin.readLineSync().toString());
+    print('Enter The Number2: ');
+    y = int.parse(stdin.readLineSync().toString());
+
     switch (opr) {
       case '+':
         result = x + y;
@@ -51,7 +69,8 @@ void calculator(opr) {
       ///////////////
       case '/':
         result = x / y;
-        break;
+        throw Error.divibyzero;
+
       ///////////////////////////
       case '%':
         result = x % y;
@@ -61,5 +80,13 @@ void calculator(opr) {
         print('رمز العملية غير صحيح!');
     }
     print('The result is $result');
+  } catch (e) {
+    print(e);
+    calculator(opr);
   }
+}
+
+class Error implements Exception {
+ static String err = "Error in this opration";
+ static String divibyzero = "cant division By Zero\n try again";
 }
